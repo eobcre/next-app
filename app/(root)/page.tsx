5a6981d01550c3493c3d72fc9671d53fc3373cab
Emmy Manning <emswoo@EM.local>
2024-12-06 13:21:11 -0500

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h1 className='text-2xl p-4'>Home</h1>
+    </>
+  );
+};
+
+export default Home;
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { auth, signIn, signOut } from '@/auth';
+import React from 'react';
+
+const NavBar = async () => {
+  const session = await auth();
+
+  return (
+    <div className='px-5 py-3'>
+      <header>
+        <nav className='flex justify-between items-center'>
+          <Link href='/'>
+            <Image src='/logo.svg' alt='logo' width={50} height={50}></Image>
+          </Link>
+
+          <div className='flex gap-7'>
+            {session && session?.user ? (
+              <>
+                <Link href='/'>
+                  <span>Create</span>
+                </Link>
+
+                <form
+                  action={async () => {
+                    'use server';
+
+                    await signOut({ redirectTo: '/' });
+                  }}
+                >
+                  <button type='submit'>Logout</button>
+                </form>
+
+                <Link href={`/user/${session?.id}`}>
+                  <span>{session?.user?.name}</span>
+                </Link>
+              </>
+            ) : (
+              <form
+                action={async () => {
+                  'use server';
+
+                  await signIn('github');
+                }}
+              >
+                <button type='submit'>Login</button>
+              </form>
+            )}
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
